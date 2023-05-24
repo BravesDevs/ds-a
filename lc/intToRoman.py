47e233f386bf540ee8cf41be767b8be917019a6a
Alexei <devangparekh2014@gmail.com>
2023-05-24 16:31:31 +0530

    12 - Integer to Roman

---
+++ lc/intToRoman.py
@@ -0,0 +1,34 @@
+class Solution:
+    def intToRoman(self, num):
+        intRomanDict = {
+        1000: 'M',
+        900: 'CM',
+        500: 'D',
+        400: 'CD',
+        100: 'C',
+        90: 'XC',
+        50: 'L',
+        40: 'XL',
+        10: 'X',
+        9: 'IX',
+        5: 'V',
+        4: 'IV',
+        1: 'I',
+        }
+
+        nums = list(intRomanDict.keys())
+        index=0
+        romanStr = ''
+        while num > 0:
+            charCount = num // nums[index]
+            romanStr += charCount*intRomanDict[nums[index]]
+            num-= charCount*nums[index]
+            index+=1
+        
+        return romanStr
+
+
+
+sln = Solution()
+result = sln.intToRoman(1994)
+print("Result: ",result)
