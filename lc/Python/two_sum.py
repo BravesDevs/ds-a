1d6c612e55780e989a450ff67fb1ed75e375e370
Alexei <devangparekh2014@gmail.com>
2023-05-30 16:19:43 +0530

    Two sum

--- lc/Python/two_sum.py
+++ lc/Python/two_sum.py
@@ -1,9 +1,10 @@
-nums = [2,7,11,15]
-target = 9
+class Solution:
+    def twoSum(self, nums, target):
+        for index,number in enumerate(nums):
+            if target - number in nums[index+1:]:
+                nums[index]=-99
+                return [index, nums.index(target - number)]
 
-def twoSum(nums, target):
-    for index,number in enumerate(nums):
-        if target - number in nums[index+1:]:
-            return [nums.index(number), nums.index(target - number)]
-
-print(twoSum(nums, target))
+sln = Solution()
+result = sln.twoSum([3,3], 6)
+print("Result: ",result)
