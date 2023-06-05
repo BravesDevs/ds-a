f1a77950f4923cf5c207712def225eac7a0d17f7
Alexei <devangparekh2014@gmail.com>
2023-06-05 10:52:53 +0530

    Candy

---
+++ lc/Python/candy.py
@@ -0,0 +1,15 @@
+class Solution:
+    def candy(self, ratings):
+        candies = [1] * len(ratings)
+        for i in range(1, len(ratings)):
+            if ratings[i] > ratings[i-1]:
+                candies[i] = candies[i-1] + 1
+        for i in range(len(ratings)-2, -1, -1):
+            if ratings[i] > ratings[i+1]:
+                candies[i] = max(candies[i], candies[i+1] + 1)
+        return candies
+
+
+sln = Solution()
+result = sln.candy([1, 1, 1])
+print("Result: ", result)
