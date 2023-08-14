42f22ddbbb86f75065b1242fcd9abcab76f850df
Alexei <devangparekh2014@gmail.com>
2023-08-14 15:12:22 +0530

    Revise key type

--- lc/Python/letterCombinations.py
+++ lc/Python/letterCombinations.py
@@ -1,36 +1,34 @@
 class Solution(object):
     def letterCombinations(self, digits):
         mapDictionary = {
-            1: [],
-            2: ['a','b','c'],
-            3: ['d','e','f'],
-            4: ['g','h','i'],
-            5: ['j','k','l'],
-            6: ['m','n','o'],
-            7: ['p','q','r','s'],
-            8: ['t','u','v'],
-            9: ['w','x','y','z']
+            '1': [],
+            '2': ['a', 'b', 'c'],
+            '3': ['d', 'e', 'f'],
+            '4': ['g', 'h', 'i'],
+            '5': ['j', 'k', 'l'],
+            '6': ['m', 'n', 'o'],
+            '7': ['p', 'q', 'r', 's'],
+            '8': ['t', 'u', 'v'],
+            '9': ['w', 'x', 'y', 'z']
         }
 
         if len(digits) == 0:
             return []
-        
+
         if len(digits) == 1:
-            return mapDictionary[int(digits)]
+            return mapDictionary[digits]
 
-        result = []
+        result = mapDictionary[digits[0]]
 
-        for i in range(len(digits)):
-            if i == 0:
-                result = mapDictionary[int(digits[i])]
-            else:
-                temp = []
-                for j in range(len(result)):
-                    for k in range(len(mapDictionary[int(digits[i])])):
-                        temp.append(result[j] + mapDictionary[int(digits[i])][k])
-                result = temp
+        for i in range(1, len(digits)):
+            temp = []
+            for j in range(len(result)):
+                for k in range(len(mapDictionary[digits[i]])):
+                    temp.append(result[j] + mapDictionary[digits[i]][k])
+            result = temp
 
         return result
+
 
 # Path: letterCombinations.py
 sln = Solution()
