5c3ddc8315dc3df5e11ef8d03f05cc1f26f46cbd
Alexei <devangparekh2014@gmail.com>
2023-08-26 18:03:55 -0400

    Revised amendments

---
+++ lc/Python/fourSum.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def fourSum(self, nums, target):
+        sortedLi = sorted(nums)
+        
+
+sln = Solution().fourSum([1, 0, -1, 0, -2, 2], 0)
+print(sln)
--- lc/Python/threeSum.py
+++ lc/Python/threeSum.py
@@ -5,11 +5,11 @@
 #         result = []
 #         i=0
 #         while i<=len(nums)-1 and nums[i]<=0:
-            
+
 #             if i>0 and nums[i]==nums[i-1]:
 #                 i+=1
 #                 continue
-            
+
 #             dupli = copy.deepcopy(nums)
 #             dupli[i]=-9999
 #             for j in range(i+1,len(nums)):
@@ -25,33 +25,30 @@
 #         return result
 
 class Solution:
-    def threeSum(self,nums):
+    def threeSum(self, nums):
         res = []
         nums.sort()
 
-        for index,element in enumerate(nums):
-            if index>0 and element == nums[index-1]:
+        for index, element in enumerate(nums):
+            if index > 0 and element == nums[index-1]:
                 continue
 
-            l,r=index+1,len(nums)-1
+            l, r = index+1, len(nums)-1
 
-            while l<r:
+            while l < r:
                 summ = element+nums[l]+nums[r]
 
-                if summ==0:
-                    res.append([element,nums[l],nums[r]])
-                    l+=1
-                elif summ<0:
-                    l+=1
+                if summ == 0:
+                    res.append([element, nums[l], nums[r]])
+                    l += 1
+                elif summ < 0:
+                    l += 1
                 else:
-                    r-=1
+                    r -= 1
         return res
+
 
 sln = Solution()
 
-result = sln.threeSum([0,0,0])
+result = sln.threeSum([0, 0, 0])
 print(result)
-
-
-
-
