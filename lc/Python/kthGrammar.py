5f3cc642bc126dfe6cc855ea85268bd9b6644c4d
Alexei <devangparekh2014@gmail.com>
2023-10-26 12:45:41 -0400

    Magical Kth Grammar

---
+++ lc/Python/kthGrammar.py
@@ -0,0 +1,19 @@
+import copy
+import numpy as np
+
+
+class Solution(object):
+    def kthGrammar(self, n, k):
+        print(n, "=>", k)
+        if n == 1 and k == 1:
+            return 0
+        else:
+            mid = pow(2, n-1)/2
+            if (k <= mid):
+                return self.kthGrammar(n-1, k)
+            else:
+                return int(not self.kthGrammar(n-1, k-mid))
+
+
+sln = Solution()
+print(sln.kthGrammar(2, 2))
