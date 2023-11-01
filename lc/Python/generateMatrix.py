fecba44bf1a9497fff4c669ee27ed3e5aed56a58
Alexei <devangparekh2014@gmail.com>
2023-10-31 22:23:24 -0400

    Spiral Order Matrix IV

--- lc/Python/generateMatrix.py
+++ lc/Python/generateMatrix.py
@@ -3,38 +3,37 @@ import itertools
 
 class Solution:
 
-    def generateMatrix(self, n):
-
-        matrix = [[None for i in range(n)] for i in range(n)]
-        seen = [[0 for i in range(n)] for i in range(n)]
-        # Traversal using simulation method
+    def generateMatrix(self, m, n, elements):
+        result = [[-1 for i in range(n)] for i in range(m)]
+        seen = [[0 for i in range(n)] for i in range(m)]
+        dir_row = [0, 1, 0, -1]
+        dir_col = [1, 0, -1, 0]
+        dir_ind = 0
 
         x, y = 0, 0
-        dir_r = [0, 1, 0, -1]
-        dir_c = [1, 0, -1, 0]
 
-        dir_ind = 0
-        count = 1
-        for i in range(n*n):
-            matrix[x][y] = count
+        for i in range(m*n):
+
+            if i >= len(elements):
+                return result
+
+            result[x][y] = elements[i]
             seen[x][y] = True
 
-            cr = x+dir_r[dir_ind]
-            cc = y+dir_c[dir_ind]
+            cal_row = x + dir_row[dir_ind]
+            cal_col = y + dir_col[dir_ind]
 
-            if 0 <= cr and cr < n and 0 <= cc and cc < n and not (seen[cr][cc]):
-                x = cr
-                y = cc
+            if 0 <= cal_row and cal_row < m and 0 <= cal_col and cal_col < n and not (seen[cal_row][cal_col]):
+                x = cal_row
+                y = cal_col
 
             else:
                 dir_ind = (dir_ind+1) % 4
-                x += dir_r[dir_ind]
-                y += dir_c[dir_ind]
-
-            count += 1
-        return matrix
+                x += dir_row[dir_ind]
+                y += dir_col[dir_ind]
+        return result
 
 
 sln = Solution()
 
-print(sln.generateMatrix(3))
+print(sln.generateMatrix(3, 5, [3, 0, 2, 6, 8, 1, 7, 9, 4, 2, 5, 5, 0]))
