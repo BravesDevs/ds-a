03cba3712148afe04336edbb96ac157c9916dae9
Alexei <devangparekh2014@gmail.com>
2023-11-13 22:31:56 -0500

    Two Sum using Hashmap

--- lc/Python/two_sum.py
+++ lc/Python/two_sum.py
@@ -1,10 +1,26 @@
 class Solution:
+    # ! Not Optimised Solution
+    # def twoSum(self, nums, target):
+    #     i = 0
+    #     while i < len(nums):
+    #         j = i+1
+    #         while j < len(nums):
+    #             if nums[j]+nums[i] == target:
+    #                 return [i, j]
+    #             else:
+    #                 j += 1
+    #         i += 1
+
     def twoSum(self, nums, target):
-        for index,number in enumerate(nums):
-            if target - number in nums[index+1:]:
-                nums[index]=-99
-                return [index, nums.index(target - number)]
+        hashMap = {}
+
+        for index, element in enumerate(nums):
+            if hashMap.get(target-element) is None:
+                hashMap[element] = index
+            else:
+                return [hashMap[target-element], index]
+
 
 sln = Solution()
-result = sln.twoSum([3,3], 6)
-print("Result: ",result)
+result = sln.twoSum([2, 7, 11, 15], 9)
+print("Result: ", result)
