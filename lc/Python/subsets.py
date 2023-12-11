7f06a89d4ff19fa0bb3e42e2f67e9a8529ce2c9c
Alexei <devangparekh2014@gmail.com>
2023-12-11 13:37:52 -0500

    Subsets

---
+++ lc/Python/subsets.py
@@ -0,0 +1,21 @@
+class Solution:
+    def subsets(self, nums):
+        res = []
+        subset = []
+
+        def dfs(i):
+            if i >= len(nums):
+                res.append(subset.copy())
+                return
+
+            subset.append(nums[i])
+            dfs(i+1)
+
+            subset.pop()
+            dfs(i+1)
+        dfs(0)
+        return res
+
+
+sln = Solution()
+print(sln.subsets([1, 2, 3]))
