692208ffe7ddc8f1cbb5bdfbd8b3ec6ed48be667
Alexei <devangparekh2014@gmail.com>
2023-12-17 16:34:56 -0500

    Four Sum

--- lc/Python/fourSum.py
+++ lc/Python/fourSum.py
@@ -1,7 +1,24 @@
 class Solution(object):
     def fourSum(self, nums, target):
-        sortedLi = sorted(nums)
-        
+        res = set()
+        nums.sort()
+        for k in range(0, len(nums)-2):
+            for i in range(k+1, len(nums)):
 
-sln = Solution().fourSum([1, 0, -1, 0, -2, 2], 0)
+                l, r = i+1, len(nums)-1
+
+                while l < r:
+                    summ = nums[k] + nums[i] + nums[l] + nums[r]
+
+                    if summ == target:
+                        res.add(tuple([nums[k], nums[i], nums[l], nums[r]]))
+                        l += 1
+                    elif summ < target:
+                        l += 1
+                    else:
+                        r -= 1
+        return list(res)
+
+
+sln = Solution().fourSum([2, 2, 2, 2, 2], 8)
 print(sln)
