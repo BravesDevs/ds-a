ba219e2dc3f8fd6c02084a17fcd8eca4b27a3959
Alexei <devangparekh2014@gmail.com>
2023-12-25 20:41:55 -0500

    Swapping nodes

--- dsa/LL/swapNodes.py
+++ dsa/LL/swapNodes.py
@@ -36,67 +36,42 @@ class Sol:
         return length
 
     def swapNodes(self, head, k):
-        first = head
-        last = head
-        count = 1
-
-        length = self.getLength(head)
-
-        while True:
-            if count < k:
-                first = first.next
-            if count <= length-k:
-                last = last.next
-            else:
-                break
-            count += 1
-        print("============================")
-        print(first.data)
-        print(last.data)
-        print("============================")
-        if first == last:
-            return head
-
+        li = []
         current = head
-
-        while current.next != first:
+        while current:
+            li.append(current.data)
             current = current.next
 
-        tempLast = copy.deepcopy(last)
-
-        last.next = first.next
-        current.next = last
-        current = current.next
-        while current.next != last:
-            current = current.next
-        first.next = tempLast.next
-        current.next = first
-
-        return head
+        li[len(li)-k], li[k-1] = li[k-1], li[len(li)-k]
+        print(li)
+        ll = ListNode(0)
+        node = ll
+        for i in li:
+            node.next = ListNode(i)
+            node = node.next
+        return ll.next
 
 
-n1 = ListNode(1)
-n2 = ListNode(2)
-# n3 = ListNode(6)
-# n4 = ListNode(6)
-# n5 = ListNode(7)
-# n6 = ListNode(8)
-# n7 = ListNode(3)
-# n8 = ListNode(0)
-# n9 = ListNode(9)
-# n10 = ListNode(5)
+n1 = ListNode(91)
+n2 = ListNode(56)
+n3 = ListNode(48)
+n4 = ListNode(77)
+n5 = ListNode(91)
+n6 = ListNode(96)
+n7 = ListNode(55)
+n8 = ListNode(53)
+n9 = ListNode(68)
 
 n1.next = n2
-# n2.next = n3
-# n3.next = n4
-# n4.next = n5
-# n5.next = n6
-# n6.next = n7
-# n7.next = n8
-# n8.next = n9
-# n9.next = n10
+n2.next = n3
+n3.next = n4
+n4.next = n5
+n5.next = n6
+n6.next = n7
+n7.next = n8
+n8.next = n9
 
 sln = Sol()
 
-ll = sln.swapNodes(n1, 1)
+ll = sln.swapNodes(n1, 7)
 print(sln.print_list(ll))
