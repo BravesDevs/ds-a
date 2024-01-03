92a01c2c29a9887d44c20e7f14cc4c917bf41743
Alexei <devangparekh2014@gmail.com>
2024-01-02 21:12:30 -0500

    Number of beams

--- lc/Python/getMinSwaps.py
+++ lc/Python/getMinSwaps.py
@@ -1,7 +1,50 @@
+import copy
+
+
 class Sol:
-    def getMinSwaps(self,num,k):
-        res = set(num)
+    def getMinSwaps(self, num, k):
+        def nextPermutation():
+            def reverse(l, r):
+                while l < r:
+                    nums[l], nums[r] = nums[r], nums[l]
+                    l += 1
+                    r -= 1
 
-        count=1
+            index1 = -1
+            for i in range(n-2, -1, -1):
+                if nums[i] < nums[i+1]:
+                    index1 = i
+                    break
 
-        while count<=k:
+            if index1 != -1:
+                index2 = -1
+                for i in range(n-1, 0, -1):
+                    if nums[i] > nums[index1]:
+                        index2 = i
+                        break
+            nums[index1], nums[index2] = nums[index2], nums[index1]
+            reverse(index1+1, n-1)
+        nums = list(num)
+        n = len(nums)
+        oldNums = nums.copy()
+        while k:
+            nextPermutation()
+            k -= 1
+        ans = 0
+
+        for i in range(n):
+            if nums[i] != oldNums[i]:
+                j = i+1
+
+            while oldNums[j] != nums[i]:
+                j += 1
+
+            while j != i:
+                oldNums[j], oldNums[j-1] = oldNums[j-1], oldNums[j]
+                j -= 1
+                ans += 1
+        return ans
+
+
+sln = Sol()
+print(sln.getMinSwaps("11112", 4))
