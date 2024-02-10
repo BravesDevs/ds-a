d395440dc4ddfcc73a872e7e90651c6ffe81ad1d
Devang MP <devangparekh2014@gmail.com>
2024-02-09 21:22:37 -0500

    Find peak

---
+++ lc/Python/findPeakElement.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findPeakElement(self, nums):
+        l = 0
+        r = len(nums)-1
+
+
+        while l < r:
+            mid = (l+r)//2
+            if nums[mid] < nums[mid+1]:
+                l = mid + 1
+            else:
+                r = mid
+        return l
+
+sln = Solution()
+print(sln.findPeakElement([1]))
