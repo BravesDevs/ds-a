a281abec3f1ce81991e6eaddbff08c3e4cef5276
Devang Parekh <devangparekh2014@gmail.com>
2024-02-24 19:48:39 -0500

    Design LRU Cache

---
+++ lc/Python/LRUCache.py
@@ -0,0 +1,17 @@
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = OrderedDict()
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            self.cache.move_to_end(key)
+            return self.cache[key]
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.cache.move_to_end(key)
+        self.cache[key] = value
+        if len(self.cache) > self.capacity:
+            self.cache.popitem(last=False)
