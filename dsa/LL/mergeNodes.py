9b36d83f377b006db2b7b07021b8d3c89e28311f
Devang Parekh <devangparekh2014@gmail.com>
2024-02-24 20:59:08 -0500

    Odd Even List

--- dsa/LL/mergeNodes.py
+++ dsa/LL/mergeNodes.py
@@ -30,15 +30,35 @@ class Solution:
         node.val = node.next.val
         node.next = node.next.next
 
+    def oddEvenList(self, head):
+        listnodes = []
 
-n1 = ListNode(0)
-n1.next = ListNode(1)
-n1.next.next = ListNode(0)
-n1.next.next.next = ListNode(3)
-n1.next.next.next.next = ListNode(0)
-n1.next.next.next.next.next = ListNode(2)
-n1.next.next.next.next.next.next = ListNode(2)
-n1.next.next.next.next.next.next.next = ListNode(0)
+        p1 = head
+
+        while p1:
+            listnodes.append(p1.val)
+            p1 = p1.next.next if p1.next else None
+
+        p2 = head.next
+
+        while p2:
+            listnodes.append(p2.val)
+            p2 = p2.next.next if p2.next else None
+        p1 = head
+        while len(listnodes):
+            p1.val = listnodes.pop(0)
+            p1 = p1.next
+        return head
+
+
+n1 = ListNode(1)
+n1.next = ListNode(2)
+n1.next.next = ListNode(3)
+n1.next.next.next = ListNode(4)
+n1.next.next.next.next = ListNode(5)
+# n1.next.next.next.next.next = ListNode(2)
+# n1.next.next.next.next.next.next = ListNode(2)
+# n1.next.next.next.next.next.next.next = ListNode(0)
 
 sln = Solution()
-sln.mergeNodes(n1)
+ll = sln.oddEvenList(n1)
