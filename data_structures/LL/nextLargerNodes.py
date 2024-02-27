02c23763c896f738446c5dad1bb16d0304c8ec26
Devang Parekh <devangparekh2014@gmail.com>
2024-02-27 15:30:19 -0500

    Next Larger Nodes

---
+++ data_structures/LL/nextLargerNodes.py
@@ -0,0 +1,40 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def nextLargerNodes(self, head):
+        if not head:
+            return
+
+        # ptr = head
+        # li = []
+        # while ptr:
+        #     li.append(ptr.val)
+        #     ptr = ptr.next
+
+        li = [1, 7, 5, 1, 9, 2, 5, 1]
+        stack = []
+
+        for i in range(len(li)):
+            while stack and li[i] > li[stack[-1]]:
+                li[stack.pop()] = li[i]
+            stack.append(i)
+
+        while stack:
+            li[stack.pop()] = 0
+
+        return li
+
+
+n1 = ListNode(2)
+n2 = ListNode(1)
+n3 = ListNode(5)
+
+n1.next = n2
+n2.next = n3
+
+sln = Solution()
+print(sln.nextLargerNodes(n1))
