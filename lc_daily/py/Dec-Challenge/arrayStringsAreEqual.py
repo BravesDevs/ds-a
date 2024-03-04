333bbfe2c39d81aaf99f70f916cd4244c6b25566
Devang Parekh <devangparekh2014@gmail.com>
2024-03-03 20:05:29 -0500

    Revision of repo

--- lc_daily/py/Dec-Challenge/arrayStringsAreEqual.py
+++
@@ -1,9 +0,0 @@
-class Sol:
-    def arrayStringsAreEqual(self, word1, word2):
-        st1 = ''.join(word1)
-        st2 = ''.join(word2)
-        return st1 == st2
-
-
-sln = Sol()
-print(sln.arrayStringsAreEqual(["abc", "d", "defg"], ["abcddefg"]))
--- lc_daily/py/Dec-Challenge/isAnagram.py
+++
@@ -1,7 +0,0 @@
-class Sol:
-    def isAnagram(self, s, t):
-        return sorted(s) == sorted(t)
-
-
-sln = Sol()
-print(sln.isAnagram("rat", "car"))
