1fff7c4aef10817143f0d4a8a4bcab369b8105ec
Devang Parekh <devangparekh2014@gmail.com>
2024-03-06 15:25:40 -0500

    Valid Palindrome II

---
+++ lc_daily/py/validPalindrome.py
@@ -0,0 +1,14 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        l, r = 0, len(s)-1
+        while l < r:
+            if s[l] != s[r]:
+                skipL, skipR = s[l+1:r+1], s[l:r]
+                return (skipL == skipL[::-1] or skipR == skipR[::-1])
+            l += 1
+            r -= 1
+        return True
+
+
+sln = Solution()
+print(sln.validPalindrome("abc"))
