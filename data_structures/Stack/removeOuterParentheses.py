c22c6a796c8839cbbe93d377955f40fa63381afb
bravesdevs <devangparekh2014@gmail.com>
2024-07-30 23:09:58 -0400

    Remove Outer Parenthesis

---
+++ data_structures/Stack/removeOuterParentheses.py
@@ -0,0 +1,23 @@
+class Solution:
+    def removeOuterParentheses(self, s):
+        stack = []
+        N = len(s)
+        shouldUse = [False]*N
+
+        for i, op in enumerate(s):
+            if op == '(':
+                stack.append(i)
+            else:
+                pop_i = stack.pop()
+                if len(stack) > 0:
+                    shouldUse[i] = True
+                    shouldUse[pop_i] = True
+        res = ""
+        for i, u in enumerate(shouldUse):
+            if u:
+                res += s[i]
+        return res
+
+
+sln = Solution()
+print(sln.removeOuterParentheses("(()())(())"))
