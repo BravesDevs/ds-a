29934b92ba9713946174134d257f9a861353120b
Devang Parekh <devangparekh@Sidneys-iMac.local>
2024-12-02 14:47:45 -0500

    Leetcode daily is_prefix_word

---
+++ lc_daily/rs/target/debug/deps/rs-2b533738e541d3fc.d
@@ -0,0 +1,5 @@
+/Users/devangparekh/Coding/Leetcode/lc_daily/rs/target/debug/deps/librs-2b533738e541d3fc.rmeta: src/main.rs
+
+/Users/devangparekh/Coding/Leetcode/lc_daily/rs/target/debug/deps/rs-2b533738e541d3fc.d: src/main.rs
+
+src/main.rs:
---
+++ lc_daily/rs/target/debug/rs.d
@@ -0,0 +1,1 @@
+/Users/devangparekh/Coding/Leetcode/lc_daily/rs/target/debug/rs: /Users/devangparekh/Coding/Leetcode/lc_daily/rs/src/main.rs
