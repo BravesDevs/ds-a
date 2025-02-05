7a59e193097dfae815b43e5ef1dea493a94f4f77
bravesdevs <devangparekh2014@gmail.com>
2025-02-05 15:19:41 -0500

    Find minimum in rotated sorted array

--- Blind75/findMin.py
+++ Blind75/findMin.py
@@ -1,19 +1,20 @@
 class Solution:
     def findMin(self, nums):
-        l,r=0,len(nums)-1
         res=nums[0]
+        l,r=0,len(nums)-1
+        
         while l<=r:
             if nums[l]<nums[r]:
                 res=min(res,nums[l])
                 break
             else:
-                mid = l+(r-l)//2
-                res = min(res,nums[mid])
-                
+                mid=(l+r)//2
                 if nums[mid]>=nums[l]:
                     l=mid+1
                 else:
                     r=mid-1
         return res
-            
                 
+                
+sln=Solution()
+print(sln.findMin([3,4,5,1,2]))
