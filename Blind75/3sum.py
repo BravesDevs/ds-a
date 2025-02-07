8cbdbbe5fe54780f798b45083523b62b1f212f96
bravesdevs <devangparekh2014@gmail.com>
2025-02-07 10:40:52 -0500

    Three sum

--- Blind75/3sum.py
+++ Blind75/3sum.py
@@ -1,5 +1,29 @@
 from collections import Counter
 class Solution:
     def threeSum(self,nums):
-        count = Counter(nums)
-        print(count)
+        nums = sorted(nums)
+        res=[]
+        for i in range(len(nums)):
+            if i > 0 and nums[i]==nums[i-1]:
+                continue
+            else:
+                l,r=i+1,len(nums)-1
+                while l<r:
+                    threeSum=nums[i]+nums[l]+nums[r]
+                    if threeSum<0:
+                        l+=1
+                    elif threeSum>0:
+                        r-=1
+                    else:
+                        res.append([nums[i],nums[l],nums[r]])
+                        l+=1
+                        while nums[l]==nums[l-1] and l<r:
+                            l+=1       
+        return res
+                
+                
+            
+        
+        
+sln = Solution()
+print(sln.threeSum([0,0,0,0]))
